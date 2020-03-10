cb6abd6595f7cadb119bdcec8ee5aff3dc5ae87c
josephw1997 <jatwinkler@gmail.com>
2020-03-11 09:58:52 +1200

    Repeat string exercise complete

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+	if (number < 0){
+		return 'ERROR';
+	}
+	let outputString = '';
+	for (let i = 0; i < number; i++){
+		outputString += word;
+	}
+	return outputString;
 }
 
 module.exports = repeatString
