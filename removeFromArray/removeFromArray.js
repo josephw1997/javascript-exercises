b89aef3964660b1db952a11cd31d358e11bcc808
josephw1997 <jatwinkler@gmail.com>
2020-03-11 10:00:08 +1200

    remove from array exercise complete, took a while

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...removables) {
+	for (i = 0; i < removables.length; i++){
+		if (originalArray.indexOf(removables[i]) < 0){
+			continue;
+		} else {
+			originalArray.splice(originalArray.indexOf(
+				removables[i]), 1);
+		}
+	}
+	return originalArray;
 }
+
+
 
 module.exports = removeFromArray
