0cfd05d19eeb99ffd64ceedc730b810b76a4d1c8
josephw1997 <jatwinkler@gmail.com>
2020-03-11 11:19:50 +1200

    temp conversion exercise complete

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+	let convertedtoC = ((ftemp - 32) * (5/9));
+	let humanC = Math.round(convertedtoC * 10) / 10; 
+	return humanC;
 }
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+	let convertedtoF = (ctemp * (9/5) + 32);
+	let humanF = Math.round(convertedtoF * 10) / 10; 
+	return humanF;
 }
 
 module.exports = {
