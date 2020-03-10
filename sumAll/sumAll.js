8b27a06b6a03df220ccbacc45193a486fb69a185
josephw1997 <jatwinkler@gmail.com>
2020-03-11 10:00:29 +1200

    sumAll exercise complete

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+	let sumFinal = 0;
+	if (firstInt < 0 || secondInt < 0){
+		return "ERROR";
+	} else if (typeof secondInt === 'string' || typeof firstInt ===
+		'string' ){
+		return "ERROR";
+	} else if (secondInt < firstInt) {
+		for (i = secondInt; i <= firstInt; i++){
+			sumFinal += i;
+		}
+	} else if (firstInt < secondInt) {
+		for (i = firstInt; i <= secondInt; i++){
+			sumFinal += i;
+		}
+	} else {
+		return "ERROR";
+	}
 
+	return sumFinal;
 }
 
 module.exports = sumAll
